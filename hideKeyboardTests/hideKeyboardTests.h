094502eef2532174499b2ba4b2b5a2635eb95627
brainelectronics <info@brainelectronics.de>
2013-04-26 22:04:16 +0200

    Initial Commit

---
+++ hideKeyboardTests/hideKeyboardTests.h
@@ -0,0 +1,13 @@
+//
+//  hideKeyboardTests.h
+//  hideKeyboardTests
+//
+//  Created by Jonas Scharpf on 26.04.13.
+//  Copyright (c) 2013 Jonas Scharpf. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface hideKeyboardTests : SenTestCase
+
+@end
---
+++ hideKeyboardTests/hideKeyboardTests.m
@@ -0,0 +1,32 @@
+//
+//  hideKeyboardTests.m
+//  hideKeyboardTests
+//
+//  Created by Jonas Scharpf on 26.04.13.
+//  Copyright (c) 2013 Jonas Scharpf. All rights reserved.
+//
+
+#import "hideKeyboardTests.h"
+
+@implementation hideKeyboardTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in hideKeyboardTests");
+}
+
+@end
